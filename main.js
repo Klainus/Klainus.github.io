cbc8720a690c9679226a348706452c8bd5d8a927
Klainus <106882227+Klainus@users.noreply.github.com>
2023-09-18 15:48:22 +0200

    pictures
    added pictures and made new code for darkmode button

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    if (element.classList.contains("dark-mode")) {
+        darkbtn.innerHTML = "Exit Dark Mode";
+    } else {
+        darkbtn.innerHTML = "Enter Dark Mode";
+    }
 }
